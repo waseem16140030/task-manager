292c71390babcc256d5ebb6aaab89ad698e5a46b
waseem16140030 <waseem16140030@gmail.com>
2025-09-11 02:43:02 +0500

    feat: added env

--- src/app/lib/customFetcher.ts
+++ src/app/lib/customFetcher.ts
@@ -1,29 +1,33 @@
 export const customFetcher = <TData, TVariables>(
   query: string,
   variables?: TVariables,
-  options?: RequestInit['headers']
+  options?: RequestInit["headers"]
 ): (() => Promise<TData>) => {
   return async () => {
-   const endPoint = "https://task-manager-rouge-seven.vercel.app/graphql"
+    const endPoint =
+      process.env.NODE_ENV === "production"
+        ? "https://task-manager-rouge-seven.vercel.app/api/graphql"
+        : "http://localhost:3000/api/graphql";
     const res = await fetch(endPoint, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        ...options
+        "Content-Type": "application/json",
+        credentials: "include",
+        ...options,
       },
       body: JSON.stringify({
         query,
-        variables
-      })
-    })
- 
-    const json = await res.json()
- 
+        variables,
+      }),
+    });
+
+    const json = await res.json();
+
     if (json.errors) {
-      const { message } = json.errors[0] || {}
-      throw new Error(message ?? 'Error…')
+      const { message } = json.errors[0] || {};
+      throw new Error(message ?? "Error…");
     }
- 
-    return json.data
-  }
-}
+
+    return json.data;
+  };
+};
